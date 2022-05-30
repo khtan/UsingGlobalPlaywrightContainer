1d91fa1161ea60fcd72207fc9b2358cf13b96fbf
KHT@air <tan.k.h@juno.com>
2022-05-30 16:59:39 -0700

    reference tankh1/pw4:v2 from Docker Hub  instead of local

--- run.bat
+++ run.bat
@@ -3,10 +3,10 @@ echo "----------------------"
 echo "--- Tests in testOnlyA"
 echo "----------------------"
 set TT=C:\cprojects\github\khtan\globalPlaywrightContainer\testOnlyA
-docker run --volume %TT%:/tests --rm --ipc=host pw4:v2
+docker run --volume %TT%:/tests --rm --ipc=host tankh1/pw4:v2
 
 echo "----------------------"
 echo "--- Tests in testOnlyB
 echo "----------------------"
 set TT=C:\cprojects\github\khtan\globalPlaywrightContainer\testOnlyB
-docker run --volume %TT%:/tests --rm --ipc=host pw4:v2
+docker run --volume %TT%:/tests --rm --ipc=host tankh1/pw4:v2
